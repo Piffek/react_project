e2b6896d7b94544ade76f8578f5506ed7b01befb
Piffek <patrykpiwko123412@gmail.com>
2017-08-26 18:59:29 +0200

    ref

--- src/components/Presentation/Banner.js
+++ src/components/Presentation/Banner.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from '../../styles/banner.scss';
+import AngleDown from 'react-icons/lib/fa/angle-down'
 
 const Banner = () => {
 	return (
 	  <div className="banner">
 		 <h1>Subject HERE</h1>
 		 <button>Zapisz się</button>
+         <button id='buttonShift'><div id="shift"><AngleDown /></div></button>
          <img></img>
       </div>
 	);
